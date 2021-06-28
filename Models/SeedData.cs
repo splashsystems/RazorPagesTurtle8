a72b630dc69d568226704695a891a7ee1b6dc11e
Scot <bscot.hagen@gmail.com>
2021-06-28 15:57:16 -0700

    Seeded database

---
+++ Models/SeedData.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using RazorPagesTurtle8.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RazorPagesTurtle8.Models
+{
+    public class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new RazorPagesTurtle8Context(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<RazorPagesTurtle8Context>>()))
+            {
+                // Look for any turtles.
+                if (context.Turtle.Any())
+                {
+                    return;   // DB has been seeded
+                }
+
+                context.Turtle.AddRange(
+                    new Turtle
+                    {
+                        TurtleName = "FlubbaDubba",
+                        ReleaseDate = DateTime.Parse("1949-8-24"),
+                        Type = "Tortoise",
+                        Price = 232.00M
+                    },
+
+                    new Turtle
+                    {
+                        TurtleName = "Chimichanga",
+                        ReleaseDate = DateTime.Parse("2015-12-24"),
+                        Type = "Sea Turtle",
+                        Price = 125.50M
+                    },
+
+                    new Turtle
+                    {
+                        TurtleName = "Swim",
+                        ReleaseDate = DateTime.Parse("2018-11-27"),
+                        Type = "Sea Turtle",
+                        Price = 58.98M
+                    },
+
+                    new Turtle
+                    {
+                        TurtleName = "Boxer",
+                        ReleaseDate = DateTime.Parse("2020-1-1"),
+                        Type = "Western Box Turtle",
+                        Price = 15.00M
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
--- Models/Turtle.cs
+++ Models/Turtle.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,8 +11,11 @@ namespace RazorPagesTurtle8.Models
     {
         public int ID { get; set; }
         public string TurtleName { get; set; }
+        [Display(Name = "Release Date")]
+        [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
         public string Type { get; set; }
+        [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
     }
 }
